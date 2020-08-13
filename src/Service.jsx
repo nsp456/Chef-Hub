1c08d6409466aa979e303456e06b8b7cf7de066a
nsp456 <61820335+nsp456@users.noreply.github.com>
2020-08-13 19:33:09 +0530

    Update Service.jsx

--- src/Service.jsx
+++ src/Service.jsx
@@ -13,15 +13,14 @@ const Service = () => {
     const [query, setQuery] = useState("");
 
     useEffect(() => {
+        const getRecipes = async () => {
+            const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+            const data = await response.json();
+            setRecipes(data.hits);
+            console.log(data.hits);
+        };
         getRecipes();
     }, [query]);
-
-    const getRecipes = async () => {
-        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-        const data = await response.json();
-        setRecipes(data.hits);
-        console.log(data.hits);
-    };
 
     const updateSearch = e => {
         setSearch(e.target.value);
